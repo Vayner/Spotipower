10f8a0e39c297eedfc79947f843387dfcf028cb8
Sverre B. Stensby <sverrebs@gmail.com>
2014-11-13 14:14:14 +0100

    now has a player, add key to keys.xml

--- app/src/main/java/com/enderwolf/spotipower/PlayerActivity.java
+++ app/src/main/java/com/enderwolf/spotipower/PlayerActivity.java
@@ -1,17 +1,48 @@
 package com.enderwolf.spotipower;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.spotify.sdk.android.Spotify;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.authentication.SpotifyAuthentication;
+import com.spotify.sdk.android.playback.ConnectionStateCallback;
+import com.spotify.sdk.android.playback.Player;
+import com.spotify.sdk.android.playback.PlayerNotificationCallback;
+import com.spotify.sdk.android.playback.PlayerState;
 
 
-public class PlayerActivity extends Activity {
+public class PlayerActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback {
+
+    // TODO: find a way to transfer this outside of git
+    //private final static String CLIENT_ID = getString(R.string.spotify_client_id);
+    //private static final String CLIENT_ID = "8cf97e259dc14327b50a3316ae6c3b60";
+    private static final String REDIRECT_URI = "spotipower-login://callback";
+
+    // Music Player
+    private Player mPlayer;
+    private PlayerState mPlayerState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        //authenticate user
+        //TODO: scopes
+        SpotifyAuthentication.openAuthWindow(getString(R.string.spotify_client_id), "token", REDIRECT_URI,
+                        new String[]{"user-read-private", "streaming"}, null, this);
+    }
+
+    public void playPause(View view) {
+        mPlayer.pause();
     }
 
 
@@ -35,5 +66,73 @@ public class PlayerActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        mPlayerState = playerState;
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onNewCredentials(String s) {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null) {
+            AuthenticationResponse response = SpotifyAuthentication.parseOauthResponse(uri);
+            Spotify spotify = new Spotify(response.getAccessToken());
+            mPlayer = spotify.getPlayer(this, "My Company Name", this, new Player.InitializationObserver() {
+                @Override
+                public void onInitialized() {
+                    mPlayer.addConnectionStateCallback(PlayerActivity.this);
+                    mPlayer.addPlayerNotificationCallback(PlayerActivity.this);
+                    //mPlayer.play("spotify:track:1mXuMM6zjPgjL4asbBsgnt");
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
     }
 }
