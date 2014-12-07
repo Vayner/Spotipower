4617670e32237349ec6cc4ab79fbe6e525623559
Tellef Møllerup Åmdal <tellefma@gmail.com>
2014-12-07 11:29:15 +0100

    Fleshed out the settings system some more.

--- app/src/main/java/com/enderwolf/spotipower/data/Settings.java
+++ app/src/main/java/com/enderwolf/spotipower/data/Settings.java
@@ -12,9 +12,13 @@ import java.util.Observable;
  * Created by !Tulingen on 07.12.2014.
  */
 public class Settings extends Observable implements Serializable {
+    public static final String FILENAME = "settings.data";
+    private static Settings settings = null;
     private Map<String, String> settingValues = new HashMap<>();
 
+    // TODO change over to objects / class hierarchy?
     private Settings () {
+        settingValues.put("Test", "false");
     }
 
     private void overwriteSettings(Settings settings) {
@@ -25,14 +29,58 @@ public class Settings extends Observable implements Serializable {
         this.notifyObservers();
     }
 
-    private static Settings settings = null;
-
-    public static void loadSettings(String fileName, Context context) {
-        getSettings().overwriteSettings(SaveSystem.<Settings>LoadData(fileName, context));
+    /**
+     *
+     * @param key to the value
+     * @return the value
+     */
+    public String get(String key) {
+        return this.settingValues.get(key);
     }
 
+    /**
+     * Checks if the key is a valid settings parameter and sets is value if it is.
+     * @param key
+     * @param value
+     */
+    public void put(String key, String value) {
+        if(this.settingValues.containsKey(key)) {
+            this.settingValues.put(key, value);
+        }
+    }
+
+    /**
+     * Loads the settings from the default file and overwrites the current settings with it.
+     * @param context
+     */
+    public static void loadSettings(Context context) {
+        loadSettings(FILENAME, context);
+    }
+
+    /**
+     * Loads the settings from file and overwrites the current settings with it.
+     * @param fileName
+     * @param context
+     */
+    public static void loadSettings(String fileName, Context context) {
+        getSettings().overwriteSettings(SaveSystem.<Settings>LoadData(FILENAME, context));
+    }
+
+    /**
+     * Saves the settings to the default file.
+     * @param context
+     */
+    public static void saveSettings(Context context) {
+        saveSettings(FILENAME, context);
+    }
+
+    /**
+     * Saves the settings to file.
+     * @param fileName
+     * @param context
+     */
     public static void saveSettings(String fileName, Context context) {
-        SaveSystem.SaveData(fileName, context, getSettings());
+        SaveSystem.SaveData(FILENAME, context, getSettings());
     }
 
     public static Settings getSettings() {
