25eb7664b447d3ec6ca5aae2c6f73cbc0cab47fe
Tellef Møllerup Åmdal <tellefma@gmail.com>
2014-12-09 14:54:49 +0100

    derp x2

--- app/src/main/java/com/enderwolf/spotipower/adapter/CustomeSongList.java
+++ app/src/main/java/com/enderwolf/spotipower/adapter/CustomeSongList.java
@@ -74,7 +74,7 @@ public class CustomeSongList extends BaseAdapter {
         Song s = songs.get(position);
 
         // thumbnail image url
-        thumbNail.setImageUrl(s.getThumbnailUrl(), imageLoader);
+        thumbNail.setImageUrl(s.getThumbnailUrl(Song.Quality.Medium), imageLoader);
 
         // title
         title.setText(s.getName());
